6593c46439b7309b4199f491f8bf71e6c11e7f03
Hiram K. <hiram.kamau@outlook.com>
2019-10-06 18:03:45 +0300

    Simplify class

--- src/main/java/com/github/idelstak/manyfacesfx/api/GlobalContext.java
+++ src/main/java/com/github/idelstak/manyfacesfx/api/GlobalContext.java
@@ -3,11 +3,7 @@
  */
 package com.github.idelstak.manyfacesfx.api;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Objects;
 import java.util.Optional;
-import java.util.Set;
 import java.util.logging.Logger;
 import org.openide.util.Lookup;
 import org.openide.util.lookup.AbstractLookup;
@@ -32,80 +28,22 @@ public abstract class GlobalContext extends ProxyLookup {
         return Optional.ofNullable(LOOKUP.lookup(GlobalContext.class)).orElse(DEFAULT);
     }
 
-    public abstract GlobalContext setLookupz(Lookup... lookups);
-
-    public abstract GlobalContext setLookup(Lookup lookup);
-
-    public abstract GlobalContext addLookup(Lookup lookup);
-
-    public abstract GlobalContext removeLookup(Lookup lookup);
-
-    public abstract GlobalContext resetLookup();
-
     public abstract <T> GlobalContext add(T inst);
 
     public abstract <T> GlobalContext remove(T inst);
 
-//    @Override
-//    protected void beforeLookup(Template<?> template) {
-//        super.beforeLookup(template);
-//        
-//        LOG.log(Level.INFO, "Before lookup: {0}", template);
-//    }
     private static final class SimpleContext extends GlobalContext {
 
         private final InstanceContent content;
-        private final Set<Lookup> localLookups = new HashSet<>();
 
         private SimpleContext() {
             this(new InstanceContent());
         }
 
         private SimpleContext(InstanceContent content) {
-            super(Lookup.EMPTY);
+            super(new AbstractLookup(content));
 
             this.content = content;
-
-            addLookup(new AbstractLookup(content));
-        }
-
-        @Override
-        public synchronized GlobalContext setLookupz(Lookup... lookups) {
-            localLookups.clear();
-            localLookups.addAll(Arrays.asList(lookups));
-            setLookups(lookups);
-            return this;
-        }
-
-        @Override
-        public synchronized GlobalContext setLookup(Lookup lookup) {
-            resetLookup();
-            addLookup(lookup);
-            return this;
-        }
-
-        @Override
-        public synchronized GlobalContext addLookup(Lookup lkp) {
-            String message = "Should not add a null lookup";
-            Lookup lookup = Objects.requireNonNull(lkp, message);
-
-            if (localLookups.add(lookup)) {
-                setLookups(localLookups.toArray(new Lookup[localLookups.size()]));
-            }
-
-            return this;
-        }
-
-        @Override
-        public synchronized GlobalContext removeLookup(Lookup lkp) {
-            String message = "Should not remove a null lookup";
-            Lookup lookup = Objects.requireNonNull(lkp, message);
-
-            if (localLookups.remove(lookup)) {
-                setLookups(localLookups.toArray(new Lookup[localLookups.size()]));
-            }
-
-            return this;
         }
 
         @Override
@@ -117,13 +55,6 @@ public abstract class GlobalContext extends ProxyLookup {
         @Override
         public synchronized <T> GlobalContext remove(T inst) {
             content.remove(inst);
-            return this;
-        }
-
-        @Override
-        public synchronized GlobalContext resetLookup() {
-            localLookups.clear();
-            setLookups(new Lookup[]{});
             return this;
         }
 
