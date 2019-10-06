6cbadb5d7f2246d3b8a5990d74fc31da7bfdb923
Hiram K. <hiram.kamau@outlook.com>
2019-10-06 04:09:27 +0300

    Add bindings

--- src/main/java/com/github/idelstak/manyfacesfx/ui/controllers/ProfileListDetailsController.java
+++ src/main/java/com/github/idelstak/manyfacesfx/ui/controllers/ProfileListDetailsController.java
@@ -5,8 +5,11 @@ package com.github.idelstak.manyfacesfx.ui.controllers;
 
 import com.github.idelstak.manyfacesfx.ui.util.TitledPaneInputEventBypass;
 import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXCheckBox;
 import com.jfoenix.controls.JFXTextField;
 import com.jfoenix.controls.JFXToggleNode;
+import javafx.beans.binding.Bindings;
+import javafx.beans.property.BooleanProperty;
 import javafx.fxml.FXML;
 import javafx.scene.control.Accordion;
 import javafx.scene.control.TitledPane;
@@ -18,12 +21,22 @@ import javafx.scene.layout.HBox;
 
  @author Hiram K <hiram.kamau@outlook.com>
  */
-public class ProfileListDetailsController  {
+public class ProfileListDetailsController {
 
     @FXML
     private TitledPane actionsPane;
     @FXML
     private HBox actionsBox;
+    @FXML
+    private JFXCheckBox selectCheckBox;
+    @FXML
+    private JFXButton selectButton;
+    @FXML
+    private JFXButton deleteButton;
+    @FXML
+    private JFXButton moveToGroupButton;
+    @FXML
+    private JFXButton removeFromGroupButton;
     @FXML
     private HBox searchPane;
     @FXML
@@ -46,6 +59,18 @@ public class ProfileListDetailsController  {
     public void initialize() {
         actionsPane.addEventFilter(InputEvent.ANY, new TitledPaneInputEventBypass());
         actionsPane.expandedProperty().bind(showActionsToggle.selectedProperty());
-    }    
-    
+        BooleanProperty checkBoxSelected = selectCheckBox.selectedProperty();
+
+        selectButton.textProperty().bind(
+                Bindings.createStringBinding(() -> selectCheckBox.isSelected()
+                                                   ? "Deselect all"
+                                                   : "Select all", checkBoxSelected));
+
+        selectButton.setOnAction(e -> selectCheckBox.setSelected(!selectCheckBox.isSelected()));
+
+        deleteButton.disableProperty().bind(checkBoxSelected.not());
+        moveToGroupButton.disableProperty().bind(checkBoxSelected.not());
+        removeFromGroupButton.disableProperty().bind(checkBoxSelected.not());
+    }
+
 }
