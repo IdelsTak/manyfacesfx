2660ebca76118ac06eadd27be4d257ca747ea17a
Hiram K. <hiram.kamau@outlook.com>
2019-10-06 18:04:54 +0300

    Remove equals() and hashCode()

--- src/main/java/com/github/idelstak/manyfacesfx/ui/MenuNode.java
+++ src/main/java/com/github/idelstak/manyfacesfx/ui/MenuNode.java
@@ -3,7 +3,6 @@
  */
 package com.github.idelstak.manyfacesfx.ui;
 
-import java.util.Objects;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.scene.Node;
 import org.openide.util.Lookup;
@@ -64,48 +63,6 @@ public class MenuNode implements Lookup.Provider {
     @Override
     public String toString() {
         return displayNameProperty.get();
-    }
-
-    @Override
-    public int hashCode() {
-        int hash = 7;
-        hash = 19 * hash + Objects.hashCode(this.content);
-        hash = 19 * hash + Objects.hashCode(this.lookup);
-        hash = 19 * hash + Objects.hashCode(this.displayNameProperty);
-        hash = 19 * hash + (this.showsNotifications ? 1 : 0);
-        hash = 19 * hash + Objects.hashCode(this.detailsPane);
-        hash = 19 * hash + Objects.hashCode(this.name);
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        final MenuNode other = (MenuNode) obj;
-        if (this.showsNotifications != other.showsNotifications) {
-            return false;
-        }
-        if (!Objects.equals(this.name, other.name)) {
-            return false;
-        }
-        if (!Objects.equals(this.content, other.content)) {
-            return false;
-        }
-        if (!Objects.equals(this.lookup, other.lookup)) {
-            return false;
-        }
-        if (!Objects.equals(this.displayNameProperty, other.displayNameProperty)) {
-            return false;
-        }
-        return Objects.equals(this.detailsPane, other.detailsPane);
     }
 
 }
