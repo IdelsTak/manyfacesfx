15006820c49f7e1ba729a2ba20e3b88437c39005
Hiram K. <hiram.kamau@outlook.com>
2019-10-12 18:08:09 +0300

    Add actions

--- src/main/java/com/github/idelstak/manyfacesfx/ui/controllers/ProfileAttributesController.java
+++ src/main/java/com/github/idelstak/manyfacesfx/ui/controllers/ProfileAttributesController.java
@@ -3,14 +3,20 @@
  */
 package com.github.idelstak.manyfacesfx.ui.controllers;
 
+import com.github.idelstak.manyfacesfx.api.GlobalContext;
+import com.github.idelstak.manyfacesfx.ui.AppMenu;
+import com.github.idelstak.manyfacesfx.ui.MenuNode;
 import java.io.IOException;
+import java.util.Iterator;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.Priority;
-import javafx.scene.layout.VBox;
+import org.openide.util.Lookup;
 
 /**
  FXML Controller class
@@ -19,10 +25,16 @@ import javafx.scene.layout.VBox;
  */
 public class ProfileAttributesController {
 
+    private static final GlobalContext CONTEXT = GlobalContext.getDefault();
     @FXML
     private HBox attributesBox;
     @FXML
-    private VBox detailsBox;
+    private AnchorPane detailsBox;
+    private final Lookup.Result<MenuNode> lookupResult;
+
+    {
+        lookupResult = CONTEXT.lookupResult(MenuNode.class);
+    }
 
     /**
      Initializes the controller class.
@@ -36,6 +48,29 @@ public class ProfileAttributesController {
         Platform.runLater(() -> {
             attributesBox.getChildren().add(1, pane);
             HBox.setHgrow(pane, Priority.NEVER);
+        });
+
+        lookupResult.addLookupListener(e -> {
+            Iterator<? extends MenuNode> it = lookupResult.allInstances().iterator();
+
+            if (it.hasNext()) {
+                MenuNode node = it.next();
+                
+                if (node.getMenu()!= AppMenu.PROFILE) {
+                    return;
+                }
+                
+                Node detailsPane = node.getDetailsPane();
+
+                Platform.runLater(() -> {
+                    detailsBox.getChildren().setAll(detailsPane);
+
+                    AnchorPane.setTopAnchor(detailsPane, 0.0);
+                    AnchorPane.setRightAnchor(detailsPane, 0.0);
+                    AnchorPane.setBottomAnchor(detailsPane, 0.0);
+                    AnchorPane.setLeftAnchor(detailsPane, 0.0);
+                });
+            }
         });
     }
 
--- src/main/java/com/github/idelstak/manyfacesfx/ui/controllers/ProfileMenuController.java
+++ src/main/java/com/github/idelstak/manyfacesfx/ui/controllers/ProfileMenuController.java
@@ -3,14 +3,23 @@
  */
 package com.github.idelstak.manyfacesfx.ui.controllers;
 
+import com.github.idelstak.manyfacesfx.api.GlobalContext;
+import com.github.idelstak.manyfacesfx.ui.AppMenu;
 import com.github.idelstak.manyfacesfx.ui.HomeNodeContext;
+import com.github.idelstak.manyfacesfx.ui.MenuNode;
 import com.github.idelstak.manyfacesfx.ui.util.TitledPaneInputEventBypass;
 import com.jfoenix.controls.JFXButton;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TitledPane;
 import javafx.scene.input.InputEvent;
+import org.openide.util.Lookup;
 
 /**
  FXML Controller class
@@ -19,6 +28,7 @@ import javafx.scene.input.InputEvent;
  */
 public class ProfileMenuController {
 
+    private static final GlobalContext CONTEXT = GlobalContext.getDefault();
     @FXML
     private JFXButton goHomeButton;
     @FXML
@@ -51,15 +61,37 @@ public class ProfileMenuController {
     private RadioButton browserPluginsToggle;
     @FXML
     private RadioButton otherToggle;
+    private final Lookup.Result<MenuNode> lookupResult;
+
+    {
+        lookupResult = CONTEXT.lookupResult(MenuNode.class);
+    }
 
     /**
      Initializes the controller class.
+
+     @throws java.io.IOException
      */
     @FXML
-    public void initialize() {
+    public void initialize() throws IOException {
+        MenuNode overviewNode = new MenuNode(
+                "overview",
+                overviewToggle.getText(),
+                false,
+                FXMLLoader.load(getClass().getResource("/fxml/ProfileOverview.fxml")),
+                AppMenu.PROFILE);
+        
+        Set<MenuNode> nodes = new HashSet<>();
+
+        nodes.add(overviewNode);
+        
+        overviewToggle.setOnAction(e -> nodes.stream().forEach(n -> switchContext(n, overviewNode)));
+        
+        ensureOverviewSelected();
+
         advancedMenuTitledPane.addEventFilter(InputEvent.ANY, new TitledPaneInputEventBypass());
         advancedMenuTitledPane.expandedProperty().bind(advancedMenuToggle.selectedProperty());
-        
+
         goHomeButton.setOnAction(e -> switchToHome());
 
     }
@@ -71,4 +103,18 @@ public class ProfileMenuController {
         Platform.runLater(() -> advancedMenuToggle.setSelected(false));
     }
 
+    private void ensureOverviewSelected() {
+        Platform.runLater(() -> {
+//            overviewToggle.fireEvent(new ActionEvent());
+//            overviewToggle.setSelected(true);
+        });
+    }
+
+    private void switchContext(MenuNode n1, MenuNode n2) {
+        if (Objects.equals(n1, n2)) {
+            CONTEXT.add(n1);
+        } else {
+            CONTEXT.remove(n1);
+        }
+    }
 }
