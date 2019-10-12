a938c3b82251e570ae088a665b88c64a618fc160
Hiram K. <hiram.kamau@outlook.com>
2019-10-12 17:33:37 +0300

    Load profile summary pane

--- src/main/java/com/github/idelstak/manyfacesfx/ui/controllers/ProfileAttributesController.java
+++ src/main/java/com/github/idelstak/manyfacesfx/ui/controllers/ProfileAttributesController.java
@@ -3,8 +3,14 @@
  */
 package com.github.idelstak.manyfacesfx.ui.controllers;
 
+import java.io.IOException;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
 
 /**
  FXML Controller class
@@ -14,14 +20,23 @@ import javafx.scene.layout.HBox;
 public class ProfileAttributesController {
 
     @FXML
-    private HBox detailsBox;
+    private HBox attributesBox;
+    @FXML
+    private VBox detailsBox;
 
     /**
      Initializes the controller class.
+
+     @throws java.io.IOException
      */
     @FXML
-    public void initialize() {
-        // TODO
+    public void initialize() throws IOException {
+        Pane pane = FXMLLoader.load(getClass().getResource("/fxml/ProfileSummary.fxml"));
+
+        Platform.runLater(() -> {
+            attributesBox.getChildren().add(1, pane);
+            HBox.setHgrow(pane, Priority.NEVER);
+        });
     }
 
 }
