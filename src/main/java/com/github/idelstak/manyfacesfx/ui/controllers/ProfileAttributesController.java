79ca30f9ddb800780d6db187638dd2d563eed14d
Hiram K. <hiram.kamau@outlook.com>
2019-10-12 17:18:51 +0300

    Initial commit

---
+++ src/main/java/com/github/idelstak/manyfacesfx/ui/controllers/ProfileAttributesController.java
@@ -0,0 +1,27 @@
+/*
+ Copyright 2019.
+ */
+package com.github.idelstak.manyfacesfx.ui.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.HBox;
+
+/**
+ FXML Controller class
+
+ @author Hiram K <hiram.kamau@outlook.com>
+ */
+public class ProfileAttributesController {
+
+    @FXML
+    private HBox detailsBox;
+
+    /**
+     Initializes the controller class.
+     */
+    @FXML
+    public void initialize() {
+        // TODO
+    }
+
+}
