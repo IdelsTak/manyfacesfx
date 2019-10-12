2afa34f95904bcff4d2e13ba6f9a1764e3a6f14e
Hiram K. <hiram.kamau@outlook.com>
2019-10-12 16:10:45 +0300

    Add tab expand binding

--- src/main/java/com/github/idelstak/manyfacesfx/ui/controllers/ProfileMenuController.java
+++ src/main/java/com/github/idelstak/manyfacesfx/ui/controllers/ProfileMenuController.java
@@ -4,9 +4,13 @@
 package com.github.idelstak.manyfacesfx.ui.controllers;
 
 import com.github.idelstak.manyfacesfx.ui.HomeNodeContext;
+import com.github.idelstak.manyfacesfx.ui.util.TitledPaneInputEventBypass;
 import com.jfoenix.controls.JFXButton;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
-import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TitledPane;
+import javafx.scene.input.InputEvent;
 
 /**
  FXML Controller class
@@ -18,7 +22,9 @@ public class ProfileMenuController {
     @FXML
     private JFXButton goHomeButton;
     @FXML
-    private ToggleGroup profileMenuGroup;
+    private TitledPane advancedMenuTitledPane;
+    @FXML
+    private RadioButton advancedMenuToggle;
 
     /**
      Initializes the controller class.
@@ -27,7 +33,14 @@ public class ProfileMenuController {
     public void initialize() {
         goHomeButton.setOnAction(e -> {
             new HomeNodeContext().refreshContext();
+            //Collapse the advanced menu when
+            //the edit profile page is exited
+            Platform.runLater(() -> advancedMenuToggle.setSelected(false));
         });
+
+        advancedMenuTitledPane.addEventFilter(InputEvent.ANY, new TitledPaneInputEventBypass());
+        
+        advancedMenuTitledPane.expandedProperty().bind(advancedMenuToggle.selectedProperty());
     }
 
 }
