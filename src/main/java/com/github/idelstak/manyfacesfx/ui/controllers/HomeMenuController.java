4d5570b6e110cf9f6aa31af6cbcbca4c86697afe
Hiram K. <hiram.kamau@outlook.com>
2019-10-12 15:53:11 +0300

    Add open edit profile page action

--- src/main/java/com/github/idelstak/manyfacesfx/ui/controllers/HomeMenuController.java
+++ src/main/java/com/github/idelstak/manyfacesfx/ui/controllers/HomeMenuController.java
@@ -4,9 +4,12 @@
 package com.github.idelstak.manyfacesfx.ui.controllers;
 
 import com.github.idelstak.manyfacesfx.api.GlobalContext;
+import com.github.idelstak.manyfacesfx.ui.AppMenu;
+import com.github.idelstak.manyfacesfx.ui.HomeNodeContext;
 import com.github.idelstak.manyfacesfx.ui.MenuNode;
 import java.io.IOException;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Objects;
 import java.util.Set;
 import java.util.logging.Logger;
@@ -16,6 +19,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.ToggleGroup;
+import org.openide.util.Lookup;
 
 /**
  FXML Controller class
@@ -38,6 +42,11 @@ public class HomeMenuController {
     private RadioButton pluginsToggle;
     @FXML
     private RadioButton helpToggle;
+    private final Lookup.Result<MenuNode> lookupResult;
+
+    {
+        lookupResult = CONTEXT.lookupResult(MenuNode.class);
+    }
 
     /**
      Initializes the controller class.
@@ -46,39 +55,61 @@ public class HomeMenuController {
      */
     @FXML
     public void initialize() throws IOException {
-        MenuNode homeNode = new MenuNode(
-                "home",
-                homeToggle.getText(),
-                true,
-                FXMLLoader.load(getClass().getResource("/fxml/HomeDetails.fxml")));
+        MenuNode homeNode = new HomeNodeContext().getNode();
         MenuNode pluginsNode = new MenuNode(
                 "plugins",
                 pluginsToggle.getText(),
                 true,
-                FXMLLoader.load(getClass().getResource("/fxml/Plugins.fxml")));
+                FXMLLoader.load(getClass().getResource("/fxml/Plugins.fxml")),
+                AppMenu.HOME);
         MenuNode myAccountNode = new MenuNode(
                 "my account",
                 myAccountToggle.getText(),
                 true,
-                FXMLLoader.load(getClass().getResource("/fxml/AccountPreferencesTab.fxml")));
+                FXMLLoader.load(getClass().getResource("/fxml/AccountPreferencesTab.fxml")),
+                AppMenu.HOME);
         MenuNode helpNode = new MenuNode(
                 "help & support",
                 helpToggle.getText(),
                 true,
-                FXMLLoader.load(getClass().getResource("/fxml/HelpAndSupport.fxml")));
+                FXMLLoader.load(getClass().getResource("/fxml/HelpAndSupport.fxml")),
+                AppMenu.HOME);
+        MenuNode newProfileNode = new MenuNode(
+                "new profile",
+                newProfileToggle.getText(),
+                true,
+                FXMLLoader.load(getClass().getResource("/fxml/ProfileSummary.fxml")),
+                AppMenu.PROFILE);
 
         Set<MenuNode> nodes = new HashSet<>();
 
         nodes.add(homeNode);
+        nodes.add(newProfileNode);
         nodes.add(myAccountNode);
         nodes.add(pluginsNode);
         nodes.add(helpNode);
 
         homeToggle.setOnAction(e -> nodes.stream().forEach(n -> context(n, homeNode)));
+        newProfileToggle.setOnAction(e -> nodes.stream().forEach(n -> context(n, newProfileNode)));
         myAccountToggle.setOnAction(e -> nodes.stream().forEach(n -> context(n, myAccountNode)));
         pluginsToggle.setOnAction(e -> nodes.stream().forEach(n -> context(n, pluginsNode)));
         helpToggle.setOnAction(e -> nodes.stream().forEach(n -> context(n, helpNode)));
 
+        ensureHomeSelected();
+
+        lookupResult.addLookupListener(e -> {
+            Iterator<? extends MenuNode> it = lookupResult.allInstances().iterator();
+
+            if (it.hasNext()
+                    && it.next()
+                            .getName()
+                            .equals(newProfileNode.getName())) {
+                Platform.runLater(() -> homeToggle.setSelected(true));
+            }
+        });
+    }
+
+    private void ensureHomeSelected() {
         Platform.runLater(() -> {
             homeToggle.fireEvent(new ActionEvent());
             homeToggle.setSelected(true);
