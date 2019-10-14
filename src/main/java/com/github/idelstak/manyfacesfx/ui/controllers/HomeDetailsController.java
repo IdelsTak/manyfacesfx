8ef44f0426dfa433e073c506db1cea6dcd90c9e6
Hiram K. <hiram.kamau@outlook.com>
2019-10-14 17:31:24 +0300

    Refactor methods

--- src/main/java/com/github/idelstak/manyfacesfx/ui/controllers/HomeDetailsController.java
+++ src/main/java/com/github/idelstak/manyfacesfx/ui/controllers/HomeDetailsController.java
@@ -5,10 +5,16 @@ package com.github.idelstak.manyfacesfx.ui.controllers;
 
 import com.github.idelstak.manyfacesfx.api.AbstractProfileListDetails;
 import com.github.idelstak.manyfacesfx.api.GlobalContext;
+import com.github.idelstak.manyfacesfx.model.Group;
 import com.github.idelstak.manyfacesfx.ui.MenuNode;
 import com.jfoenix.controls.JFXTabPane;
 import java.io.IOException;
+import java.net.URL;
 import java.util.Iterator;
+import java.util.Optional;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
@@ -22,6 +28,8 @@ import org.openide.util.Lookup;
  */
 public class HomeDetailsController {
 
+    private static final Logger LOG = Logger.getLogger(HomeDetailsController.class.getName());
+    private static final GlobalContext CONTEXT = GlobalContext.getDefault();
     @FXML
     private JFXTabPane detailsTabPane;
     @FXML
@@ -30,10 +38,12 @@ public class HomeDetailsController {
     private Tab groupsTab;
     private Node groupsContent;
     private MenuNode homeNode;
-    private final Lookup.Result<MenuNode> lookupResult;
+    private final Lookup.Result<MenuNode> menuNodeResult;
+    private final Lookup.Result<Group> groupResult;
 
     {
-        lookupResult = GlobalContext.getDefault().lookupResult(MenuNode.class);
+        menuNodeResult = CONTEXT.lookupResult(MenuNode.class);
+        groupResult = CONTEXT.lookupResult(Group.class);
     }
 
     /**
@@ -51,8 +61,8 @@ public class HomeDetailsController {
                     }
                 });
 
-        lookupResult.addLookupListener(e -> {
-            Iterator<? extends MenuNode> it = lookupResult.allInstances().iterator();
+        menuNodeResult.addLookupListener(e -> {
+            Iterator<? extends MenuNode> it = menuNodeResult.allInstances().iterator();
 
             if (it.hasNext()) {
                 MenuNode node = it.next();
@@ -63,6 +73,8 @@ public class HomeDetailsController {
                 }
             }
         });
+
+        groupResult.addLookupListener(e -> showProfilesByGroup());
 
         profileListTab.setContent(AbstractProfileListDetails.getDefault().getPane());
         groupsTab.setContent(getGroupsTab());
@@ -76,5 +88,33 @@ public class HomeDetailsController {
 
     private Node getGroupsTab() throws IOException {
         return FXMLLoader.load(getClass().getResource("/fxml/GroupListDetails.fxml"));
+    }
+
+    private Optional<Node> getProfilesListByGroupContent(Group group) {
+        URL location = getClass().getResource("/fxml/ProfileListDetails.fxml");
+        FXMLLoader loader = new FXMLLoader(location);
+        Optional<Node> content = Optional.empty();
+
+        try {
+            content = Optional.ofNullable(loader.load());
+            ProfileListDetailsController controller = loader.getController();
+            controller.setGroup(group);
+        } catch (IOException ex) {
+            LOG.log(Level.SEVERE, null, ex);
+        }
+
+        return content;
+    }
+
+    private void showProfilesByGroup() {
+        Iterator<? extends Group> it = groupResult.allInstances().iterator();
+
+        if (it.hasNext()) {
+
+            getProfilesListByGroupContent(it.next())
+                    .ifPresent(node -> {
+                        Platform.runLater(() -> groupsTab.setContent(node));
+                    });
+        }
     }
 }
